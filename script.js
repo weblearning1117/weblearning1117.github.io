671ec2ceece3d1526c75969558ca73b6717903a7
weblearning1117 <46844775+weblearning1117@users.noreply.github.com>
2019-10-29 15:56:31 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -37,14 +37,6 @@ $(document).ready(function () {
             }
     }, 432);
     
-    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    if(!isChrome){
-      $('#iframeAudio').remove()
-    }
-  else{
-     $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background 
-  }
-   
 
     
     
