cc8f197ba5543aefd5125a77802187547b9143c1
weblearning1117 <46844775+weblearning1117@users.noreply.github.com>
2019-10-30 17:08:35 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    var winWid = window.innerWidth;
+    var ghost = $('.ghost.active');
+    var pumpkin = $('.pumpkin.active');
+    var man = $('.man.active');
+    var count = $('.point');
+    var x, y, ghostie;
+
+    spawn(ghost, 100, 850, false);
+    spawn(pumpkin, 500, 5000, 'pump');
+    spawn(man, -1000, 3000, 'man');
+  
+  
+    function spawn(element, point, time, fade) {
+      setInterval(() => {
+          if ($('.ghost.ghostie').length > 14) {
+              return false;
+          } 
+          else {
+                     x = Math.round(10 - 0.5 + Math.random() * (90 - 10 + 1));
+        y = Math.round(10 - 0.5 + Math.random() * (90 - 10 + 1));
+        element.clone().appendTo('.game').css('left', x + '%').css('top', y + '%').addClass('ghostie');
+        ghostie = $('.ghostie');
+  
+        ghostie.on('click', function () {
+          if ($(this).hasClass('off')) {
+            return false;
+          } else {
+            navigator.vibrate(1);
+            number_to('point', parseInt(count.html()), parseInt(count.html()) + point, 100);
+            $(this).removeClass('active').addClass('off');
+            $(this).slideUp(200);
+            setTimeout(() => {
+              $(this).remove();
+            }, 200);
+  
+          }
+  
+        })
+        if (fade == 'pump') {
+          setTimeout(() => {
+            $('.pumpkin.ghostie').slideUp(200);
+            setTimeout(() => {
+              $('.pumpkin.ghostie').remove();
+            }, 200);
+          }, 2000);
+        } else if (fade == 'man') {
+          setTimeout(() => {
+            $('.man.ghostie').slideUp(200);
+            setTimeout(() => {
+              $('.man.ghostie').remove();
+            }, 200);
+          }, 2000);
+        }
+  
+          }
+ 
+  
+      }, time);
+   
+    }
+  
+  
+  
+  
+  
+  
+    function number_to(id, from, to, duration) {
+      var element = document.getElementById(id);
+      var start = new Date().getTime();
+      setTimeout(function () {
+        var now = (new Date().getTime()) - start;
+        var progress = now / duration;
+        var result = Math.floor((to - from) * progress + from);
+        element.innerHTML = progress < 1 ? result : to;
+        if (progress < 1) setTimeout(arguments.callee, 10);
+      }, 10);
+    }
+  });
--- script.js
+++ script.js
@@ -1,72 +1,20 @@
 $(document).ready(function () {
-    var winWid = window.innerWidth;
-    var ghost  = $('.ghost.active');
-    var count  = $('.point');
-    var x, y, ghostie;
-    document.onselectstart = function() {
-      return false;
+  document.onselectstart = function () {
+    return false;
   };
-    document.oncontextmenu = function() {
-      return false;
+  document.oncontextmenu = function () {
+    return false;
   };
-
-    setInterval(() => {
-      x = Math.round(5 - 0.5 + Math.random() * (95 - 5 + 1));
-      y = Math.round(5 - 0.5 + Math.random() * (95 - 5 + 1));
-      ghost.clone().appendTo('.game').css('left', x + '%').css('top', y + '%').addClass('ghostie');
-            ghostie = $('.ghostie');
-
-              ghostie.on('click', function () {
-                if ($(this).hasClass('off')) {
-                  return false;
-                }
-                else {
-                      navigator.vibrate(1);
-                number_to('point', parseInt(count.html()), parseInt(count.html()) + 100, 100);
-                $(this).removeClass('active').addClass('off');
-                if (x <= 50) {
-                  $(this).fadeOut(200);
-                }
-                else {
-                  $(this).slideUp(200);
-                }
-       
-               
-                setTimeout(() => {
-                  $(this).remove();
-                }, 200);
-                }
-            
-              })
-              
-             
-           
-    }, 850);
-   
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-function number_to(id,from,to,duration) {
-  var element = document.getElementById(id);
-  var start = new Date().getTime();
-  setTimeout(function() {
-  var now = (new Date().getTime()) - start;
-  var progress = now / duration;
-  var result = Math.floor((to - from) * progress + from);
-  element.innerHTML = progress < 1 ? result : to;
-  if (progress < 1) setTimeout(arguments.callee, 10);
-  }, 10);}
+  $('.btn').click(function () {
+    $('.hello').fadeOut(500);
+    setTimeout(() => {
+      include('./game.js');
+    }, 500);
+  })
+  function include(url) {
+    var script = document.createElement('script');
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+    script.className += "game-js";
+}
 });
